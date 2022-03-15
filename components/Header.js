6ff555e3905b0326fee4ed4fb672af6bead4c56a
zhangyunzhi-web <318866560@qq.com>
2022-03-15 18:03:10 +0800

    头部组件样式修改

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ import {Row,Col, Menu, Icon} from 'antd'
 const Header = () => (
   <div className="header">
     <Row type="flex" justify="center">
-        <Col  xs={24} sm={24} md={10} lg={10} xl={10}>
+        <Col  xs={24} sm={24} md={10} lg={15} xl={12}>
             <span className="header-logo">Zyz</span>
             <span className="header-txt">就是个敲代码的。</span>
         </Col>
